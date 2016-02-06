996cae4e2a4c76a1a3242703874057ee8f4e232d
jamiesun <jamiesun.net@gmail.com>
2016-02-06 23:19:13 +0800

    update message stat

--- toughtester/admin/account/sessions.py
+++ toughtester/admin/account/sessions.py
@@ -2,12 +2,17 @@
 # coding:utf-8
 
 import cyclone.web
-from toughlib import utils
+from toughlib import utils,logger
 from toughtester.admin.base import BaseHandler, MenuUser
 from toughlib.permit import permit
-from twisted.internet import defer
+from twisted.internet import defer, reactor
 from toughtester.radius.session import RadiusSession
 from toughtester import models
+
+def sleep(secs):
+    d = defer.Deferred()
+    reactor.callLater(secs, d.callback, None)
+    return d
 
 def fmt_time(times):
     d = times / (3600 * 24)
@@ -30,8 +35,27 @@ class AccountSessionHandler(BaseHandler):
 
 @permit.route(r"/account/session/stop", u"在线会话解锁", MenuUser, order=3.0001)
 class AccountSessionStopHandler(BaseHandler):
+
+    @defer.inlineCallbacks
     @cyclone.web.authenticated
     def post(self):
-        session_id = self.get_argument("session_id")
-        RadiusSession.stop_session(session_id=session_id)
+        batch = self.get_argument("batch",None)
+        session_id = self.get_argument("session_id",None)
+        randsecs = (0.001,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.10)
+        if batch:
+            for _session_id in RadiusSession.sessions.keys():
+                session = RadiusSession.sessions.pop(_session_id)
+                session.stop().addCallbacks(logger.info,logger.error)
+                yield sleep(0.01)
+        else:
+            if session_id:
+                RadiusSession.stop_session(session_id=session_id)
         self.render_json(code=0,msg=u"stop session done")
+
+
+
+
+
+
+
+
